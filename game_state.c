d2e5a283973b1d249e58a287588a1d52106e5bf4
takhdeer <tgrew803@mtroyal.ca>
2026-02-17 14:13:02 -0700

    implemented state update function & check if game over function

--- game_state.c
+++ game_state.c
@@ -5,6 +5,40 @@ GameState initial_game_state = {
     .score = 0,
     .level = 1,
     .lines_cleared = 0,
-    .is_game_over = 0
+    .is_game_over = 0,
 };
 
+UINT16 map_line_base(const UINT16 lines_cleared) {
+    if (lines_cleared == 1)
+    {
+        return SINGLE_LINE_BASE; 
+    }
+    else if (lines_cleared == 2)
+    {
+        return DOUBLE_LINE_BASE;
+    }
+    else if (lines_cleared == 3)
+    {
+        return TRIPLE_LINE_BASE;
+    }
+    else{
+        return TETRIS_LINE_BASE;
+    }
+}
+
+void update_state(GameState *state, UINT16 lines_just_cleared) {
+    UINT16 base_points = map_line_base(lines_just_cleared);
+    state->score += (base_points) * (state->level + 1);
+    state->lines_cleared += lines_just_cleared;
+    
+    if (state->lines_cleared % 10 == 0) {
+        state->level++;
+    }
+}
+
+UINT8 check_game_over(const GameState *state) {
+    if (state->is_game_over == 1) {
+        return 1;
+    }
+    return 0;
+}
--- game_state.h
+++ game_state.h
@@ -1,7 +1,12 @@
-#include "types.h"
 #ifndef GAME_STATE_H
 #define GAME_STATE_H
 #define LINES_PER_LEVEL 10
+#define SINGLE_LINE_BASE 40
+#define DOUBLE_LINE_BASE 100
+#define TRIPLE_LINE_BASE 300
+#define TETRIS_LINE_BASE 1200
+#include "types.h"
+
 typedef struct GameState {
     UINT16  score;
     UINT16  level;
@@ -11,5 +16,7 @@ typedef struct GameState {
 
 extern GameState initial_game_state;
 
+UINT16 map_line_base(const UINT16 lines_cleared);
 void update_state(GameState *state, UINT16 lines_cleared);
+UINT8 check_game_over(const GameState *state);
 #endif
