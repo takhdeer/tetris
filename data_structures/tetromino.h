a43c314d7e818ce73199b87d82e9450c7b043b54
Aaron Arrieta <aaron.arrieta0604@gmail.com>
2026-02-17 17:17:38 -0700

    Finished tetromino.h (working on tetromino.c)

---
+++ data_structures/tetromino.h
@@ -0,0 +1,36 @@
+
+// Done by Aaron
+
+// CONSTANTS
+#ifndef TETROMINO_H
+#define TETROMINO_H
+
+// Tetromino types
+#define TETROMINO_I 0
+#define TETROMINO_O 1
+#define TETROMINO_T 2
+#define TETROMINO_Z 3
+#define TETROMINO_S 4
+#define TETROMINO_L 5
+#define TETROMINO_J 6
+ 
+// Tetromino struct
+typedef struct {
+    int type;
+    int row;
+    int col;
+    int rotation;
+
+} Tetromino;
+
+extern const int tetromino_shapes[7][4][4][4];
+
+//Behaviour functions
+void init_tetromino(Tetromino *piece, int type, int spawn_col);
+void move_tetromino_left(Tetromino *piece);
+void move_tetromino_right(Tetromino *piece);
+void move_tetromino_down(Tetromino *piece);
+void rotate_tetromino_cw(Tetromino *piece);
+int get_cell(const Tetromino *piece, int local_row, int local_col);
+
+#endif 
