c730f1dac7fd9d919a6372bce84278272af0c20e
takhdeer <tgrew803@mtroyal.ca>
2026-02-18 19:24:01 -0700

    added move right and down testing

--- test_movement.c
+++ test_movement.c
@@ -6,13 +6,25 @@
 int main() {
     Model test_model;
     Model *model_pointer = &test_model;
-    printf("\n === Initializing Movement Testing \n");
+    printf("\n === Initializing Movement Testing ===\n");
     
     init_tetromino(&model_pointer->piece,TETROMINO_I,5);   // initializing tetramino to col 5
 
-    printf("=== Testing Move Left ===\n");
+    
+    printf("\n=== Testing Move Left ===\n");
     printf("Initial position - Row: %d, Col: %d\n", model_pointer->piece.row, model_pointer->piece.col);
     // move left 
     move_tetromino_left(&model_pointer->piece);
     printf("After move_left() - Row: %d, Col: %d\n", model_pointer->piece.row, model_pointer->piece.col);
+
+    // move right
+    printf("\n=== Testing Move right === \n");
+    move_tetromino_right(&model_pointer->piece);
+    printf("After move_right() - Row: %d, Col: %d\n", model_pointer->piece.row, model_pointer->piece.col);
+    
+    // move down
+    printf("\n=== Testing Move Down ===\n");
+    move_tetromino_down(&model_pointer->piece);
+    printf("After move_down() - Row: %d, Col: %d\n", model_pointer->piece.row, model_pointer->piece.col);
+
 }
