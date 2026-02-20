319ada7164e0db5d005b96b2a270af2ca7933304
takhdeer <tgrew803@mtroyal.ca>
2026-02-20 11:37:03 -0700

    added raster.c file + updated gitignore

---
+++ raster.c
@@ -0,0 +1,62 @@
+#include "raster.h"
+#include "types.h"
+
+void clear_screen(UINT32 *base) {
+
+}
+
+void clear_region(UINT32 *base, UINT16 row, UINT16 col, UINT16 length, UINT16 width) {
+
+}
+
+void plot_pixel(UINT8 *base, UINT16 row, UINT16 col) {
+
+}
+
+void plot_horizontal_line(UINT32 *base, UINT16 row, UINT16 col, UINT16 length) {
+
+}
+
+void plot_vertical_line(UINT32 *base, UINT16 row, UINT16 col, UINT16 length) {
+
+}
+
+void plot_rectangle(UINT32 *base, UINT16 row, UINT16 col, UINT16 length, UINT16 width) {
+
+}
+
+void plot_square(UINT32 *base, UINT16 row, UINT16 col, UINT16 side) {
+
+}
+
+void plot_triangle(UINT32 *base, UINT16 row, UINT16 col, UINT16 base, UINT16 height, UINT8 direction) {
+
+}
+
+void plot_square(UINT32 *base, UINT16 row, UINT16 col, UINT16 side) {
+
+}
+
+void plot_triangle(UINT32 *base, UINT16 row, UINT16 col, UINT16 base, UINT16 height, UINT8 direction){
+
+}
+
+void plot_bitmap_8(UINT8 *base, UINT16 row, UINT16 col, UINT16 height) {
+
+}
+
+void plot_bitmap_16(UINT16 *base, UINT16 row, UINT16 col, UINT16 height){
+
+}
+
+void plot_bitmap_32(UINT32 *base, UINT16 row, UINT16 col, UINT16 height){
+
+}
+
+void plot_character(UINT8 *base, UINT16 row, UINT16 col, char ch) {
+
+}
+
+void plot_string(UINT8 *base, UINT16 row, UINT16 col, char *ch){
+    
+}
