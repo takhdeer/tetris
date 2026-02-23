fcc3a7f3858f3827630dc459b7613efbd0c87678
takhdeer <tgrew803@mtroyal.ca>
2026-02-23 13:45:02 -0700

    updated documentation

--- cond.h
+++ cond.h
@@ -3,4 +3,8 @@
 #include "data_structures/synch.h"
 #include <stdio.h>
 
+/*
+    Spawns a new peice and checks for collisions. 
+    Sets is_game_over to 1 if there are collisions
+ */
 int spawn_piece(Model *model);
