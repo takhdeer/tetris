f0bdc3f248fa61b8da61f8b90dcd47ab7faedb54
Aaron Arrieta <aaron.arrieta0604@gmail.com>
2026-02-23 20:41:22 -0700

    updated test_next_box_renderer.c

--- aaron_renderer.c
+++ aaron_renderer.c
@@ -10,27 +10,6 @@
 #define NEXT_BOX_ROW 50
 #define NEXT_BOX_COL 500
 
-void render_tetromino(UINT8 *base, const Tetromino *piece) {
-    int screen_row;
-    int screen_col;
-
-    // Looping through 4x4 box
-    for (int local_row = 0 ; local_row < 4 ; local_row++) {
-        for (int local_col = 0 ; local_col < 4 ; local_col++) {
-
-             //Check if cell is occupied
-             if (get_cell(piece, local_row, local_col)) {
-
-                // Calculating screen position
-                screen_row = (piece->row + local_row) * CELL_SIZE;
-                screen_col = (piece->col + local_col) * CELL_SIZE;
-
-                plot_square((UINT32 *)base, screen_row, screen_col, CELL_SIZE);
-             }
-        }
-    }
-}
-
 void render_next_box(UINT8 *base, const NextBox *box) {
     Tetromino preview_piece;
     int next_type;
