24ab76795df2cf2909d41a80fdc869313cdca436
takhdeer <tgrew803@mtroyal.ca>
2026-02-23 20:02:03 -0700

    removed sprintf call from render function

--- takh_render.c
+++ takh_render.c
@@ -31,20 +31,82 @@ void render_peice(const Tetromino *piece, UINT32 *base) {
     }
 }
 
-void render_score(const GameState *game, UINT8 *base) { 
-    // creating arrays
-    char score_text[20];
+void uint_to_str(UINT32 value, char *buf, int start) {
+    int i = start + 5;   
+    buf[i] = '\0';
+    
+    if (value == 0) {
+        buf[--i] = '0';
+    } 
+    else {
+        while (value > 0 && i > start) {
+            buf[--i] = '0' + (value % 10);
+            value /= 10;
+        }
+    }
 
-    // converting nums into strings
-    sprintf(score_text, "SCORE: %u", game->score);
-
-    // rendering each line of text
-    plot_string(base, 10, 200, score_text);
+    /* Shift digits left to sit right after the prefix */
+    {
+        int src = i, dst = start;
+        while (buf[src] != '\0')
+            buf[dst++] = buf[src++];
+        buf[dst] = '\0';
+    }
 }
 
-void render_level(const GameState *game, UINT8 base) {
-    char level_text[20];
-    sprintf(level_text, "LEVEL: %U", game->level);
-    plot_string(base, 30, 200, level_text);
+void render_score(const GameState *game, UINT8 *base) {
+    char score_text[20];
 
+    score_text[0] = 'S';
+    score_text[1] = 'C';
+    score_text[2] = 'O';
+    score_text[3] = 'R';
+    score_text[4] = 'E';
+    score_text[5] = ':';
+    score_text[6] = ' ';
+
+    uint_to_str(game->score, score_text, 7);
+
+    plot_string(base, SCORE_Y, SCORE_X, score_text);
+}
+
+void render_lines(const GameState *game, UINT8 *base) {
+    char lines_text[20];
+
+    lines_text[0] = 'L';
+    lines_text[1] = 'I';
+    lines_text[2] = 'N';
+    lines_text[3] = 'E';
+    lines_text[4] = 'S';
+    lines_text[5] = ':';
+    lines_text[6] = ' ';
+
+    uint_to_str(game->lines_cleared, lines_text, 7);
+
+    plot_string(base, LINES_Y, LINES_X, lines_text);
+}
+
+
+void render_game_over(UINT8 *base, int score) {
+    char score_str[16];
+    int i; 
+
+    // rendering border for game over box
+    plot_rectangle((UINT32 *) base, 170, 220, 50, 200);
+
+    // rendering game over text
+    plot_string(base, 180, 240, "GAME OVER");
+
+    // Convert score to integer to string manually
+    score_str[0] = 'S';
+    score_str[1] = 'C';
+    score_str[2] = 'O';
+    score_str[3] = 'R';
+    score_str[4] = 'E';
+    score_str[5] = ':';
+    score_str[6] = ' ';
+
+    uint_to_str((UINT32) score, score_str, 7);
+
+    plot_string(base, 200, 256, score_str);
 }
--- takh_renderer.h
+++ takh_renderer.h
@@ -6,6 +6,10 @@
 
 void render_peice(const Tetromino *piece, UINT32 *base);
 
+void uint_to_str(UINT32 value, char *buf, int start);
+
 void render_score(const GameState *game, UINT8 *base);
 
 void render_level(const GameState *game, UINT8 base);
+
+void render_game_over(UINT8 *base, int score);
