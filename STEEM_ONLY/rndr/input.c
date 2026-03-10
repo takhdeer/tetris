f5da86572d464f98007fa708e3dd48695e0e258e
Aaron Arrieta <aaron.arrieta0604@gmail.com>
2026-03-09 22:35:44 -0600

    added input.h + input.c

---
+++ STEEM_ONLY/rndr/input.c
@@ -0,0 +1,18 @@
+/* File: input.c
+ * Author: Aaron
+ * Team: Aaron, Henry, Takhdeer
+ * 
+ * Purpose: input.h implementations.
+ * 
+ */
+
+#include "input.h";
+#include <osbind.h>;
+
+int has_input() {
+    return Cconis();
+}
+
+char get_input() {
+    return (char)Cnecin();
+}
---
+++ STEEM_ONLY/rndr/input.h
@@ -0,0 +1,30 @@
+/* File: input.h
+ * Author: Aaron
+ * Team: Aaron, Henry, Takhdeer
+ * 
+ * Purpose: Hardware-independent input module
+ *          Provides a middle layer between game code and OS-specific input
+ */
+
+#ifndef INPUT_H
+#define INPUT_H
+
+/*----- Function: has_input -----
+ PURPOSE: Check if keyboard input is pending
+
+ INPUT: None
+
+ OUTPUT: Returns 1 (true) if pending input is to be processed, 0 (false) otherwise
+*/
+int has_input();
+
+/*----- Function: get_input -----
+ PURPOSE: Read the next character from keyboard input
+
+ INPUT: None
+
+ OUTPUT: Returns the next character to be pressed.
+*/
+char get_input();
+
+#endif
